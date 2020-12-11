381791b5963f96826cf2ff31c36c596b1162a57d
Dustin Michels <dustin7538@gmail.com>
2020-12-11 10:02:45 +0100

    add feedback and basic CLI

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
-import pandas as pd
 import os
 from datetime import date
 
+import pandas as pd
+from termcolor import colored, cprint
 
+# CONGIG
 KEY_SPREADSHEET = "POXC_FinalData.xlsx"
 DATA_SHEET_NAME = "POX-C Calculation"
 DATA_DIR = "data"
@@ -21,6 +23,7 @@ def load_and_merge(filename, key_df):
 
 
 def save_outout(full_df):
+
     # create output folder if it doesn't exist
     try:
         os.mkdir(OUT_DIR)
@@ -31,9 +34,13 @@ def save_outout(full_df):
     today_str = date.today().strftime("%m-%d-%Y")
     filename = f"output_{today_str}.xlsx"
     full_df.to_excel(os.path.join(OUT_DIR, filename), index=False)
+    cprint("\nDone!", "green", attrs=["bold"])
+    print(f"> Saved result to file '{os.path.join(OUT_DIR, filename)}'\n")
 
 
 def main():
+    print("\n----- RUNNING -----")
+
     # load key/treatment data
     key_df = pd.read_excel(os.path.join(DATA_DIR, KEY_SPREADSHEET), header=0)
     key_df.columns = [x.lower() for x in key_df.columns]
@@ -42,8 +49,13 @@ def main():
     dfs = []
     for filename in os.listdir(DATA_DIR):
         if filename != KEY_SPREADSHEET:
-            merged_df = load_and_merge(filename, key_df)
-            dfs.append(merged_df)
+            try:
+                merged_df = load_and_merge(filename, key_df)
+                dfs.append(merged_df)
+                print(f"> Succesfully handled {filename}")
+            except:
+                print(f"> Warning! Error with {filename}")
+                pass
 
     # concatenate merged dfs into single datafame
     full_df = pd.concat(dfs)
@@ -52,5 +64,35 @@ def main():
     save_outout(full_df)
 
 
+def get_input():
+    """Prompt for input which would override default, global values"""
+
+    global DATA_DIR, OUT_DIR, KEY_SPREADSHEET, DATA_SHEET_NAME
+
+    print("\n----- CONFIG -----")
+    print("Press enter to use default values.\n")
+
+    bold = lambda x: colored(f"[{x}]", attrs=["bold"])
+
+    data_dir = input(f"Path to data directory? {bold(DATA_DIR)}: ")
+    if data_dir:
+        DATA_DIR = data_dir
+
+    out_dir = input(f"Where to output files? {bold(OUT_DIR)}: ")
+    if out_dir:
+        OUT_DIR = out_dir
+
+    key_spreadsheet = input(f"Name of the treatment sheet? {bold(KEY_SPREADSHEET)}: ")
+    if key_spreadsheet:
+        KEY_SPREADSHEET = key_spreadsheet
+
+    data_sheet_name = input(
+        f"Name of data tab within data sheets? {bold(DATA_SHEET_NAME)}: "
+    )
+    if data_sheet_name:
+        DATA_SHEET_NAME = data_sheet_name
+
+
 if __name__ == "__main__":
+    get_input()
     main()
