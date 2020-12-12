3b43630f018759ea1d10e31be474f2fa1cbdbd07
Dustin Michels <dustin7538@gmail.com>
2020-12-12 14:50:45 +0100

    tweak script

--- script.py
+++ script.py
@@ -12,7 +12,9 @@ OUT_DIR = "output"
 
 
 def load_and_merge(filename, key_df):
-    """Open a data file and merge it with key dataframe"""
+    """
+    Open a data file and merge it with key dataframe
+    """
 
     # load source data
     filepath = os.path.join(DATA_DIR, filename)
@@ -27,7 +29,9 @@ def load_and_merge(filename, key_df):
 
 
 def save_outout(full_df):
-    """Save complete DataFrame to excel file"""
+    """
+    Save complete DataFrame to excel file
+    """
 
     # create output folder if it doesn't exist
     try:
@@ -37,19 +41,50 @@ def save_outout(full_df):
 
     # save dataframe to file with today's date
     today_str = date.today().strftime("%m-%d-%Y")
-    filename = f"output_{today_str}.xlsx"
-    full_df.to_excel(os.path.join(OUT_DIR, filename), index=False)
+    # filename = f"output_{today_str}.xlsx"
+    # full_df.to_excel(os.path.join(OUT_DIR, filename), index=False)
+    filename = f"output_{today_str}.csv"
+    full_df.to_csv(os.path.join(OUT_DIR, filename), index=False)
 
     # print info
     cprint("\nDone!", "green", attrs=["bold"])
     print(f"> Saved result to file '{os.path.join(OUT_DIR, filename)}'\n")
 
 
+def get_input():
+    """
+    Prompt for input which would override default, global values
+    """
+
+    global DATA_DIR, OUT_DIR, KEY_SPREADSHEET, DATA_SHEET_NAME
+
+    boldify = lambda x: colored(f"[{x}]", attrs=["bold"])
+
+    data_dir = input(f"Path to data directory? {boldify(DATA_DIR)}: ")
+    if data_dir:
+        DATA_DIR = data_dir
+
+    out_dir = input(f"Where to output files? {boldify(OUT_DIR)}: ")
+    if out_dir:
+        OUT_DIR = out_dir
+
+    key_spreadsheet = input(
+        f"Name of the treatment sheet? {boldify(KEY_SPREADSHEET)}: "
+    )
+    if key_spreadsheet:
+        KEY_SPREADSHEET = key_spreadsheet
+
+    data_sheet_name = input(
+        f"Name of data tab within data sheets? {boldify(DATA_SHEET_NAME)}: "
+    )
+    if data_sheet_name:
+        DATA_SHEET_NAME = data_sheet_name
+
+
 def main():
     """
     Iterate over data files, merging with key data and concatenating into single DataFrame
     """
-    print("\n----- RUNNING -----")
 
     # load key/treatment data
     key_df = pd.read_excel(os.path.join(DATA_DIR, KEY_SPREADSHEET), header=0)
@@ -74,35 +109,10 @@ def main():
     save_outout(full_df)
 
 
-def get_input():
-    """Prompt for input which would override default, global values"""
-
-    global DATA_DIR, OUT_DIR, KEY_SPREADSHEET, DATA_SHEET_NAME
-
-    print("\n----- CONFIG -----")
-    print("Press enter to use default values.\n")
-
-    bold = lambda x: colored(f"[{x}]", attrs=["bold"])
-
-    data_dir = input(f"Path to data directory? {bold(DATA_DIR)}: ")
-    if data_dir:
-        DATA_DIR = data_dir
-
-    out_dir = input(f"Where to output files? {bold(OUT_DIR)}: ")
-    if out_dir:
-        OUT_DIR = out_dir
-
-    key_spreadsheet = input(f"Name of the treatment sheet? {bold(KEY_SPREADSHEET)}: ")
-    if key_spreadsheet:
-        KEY_SPREADSHEET = key_spreadsheet
-
-    data_sheet_name = input(
-        f"Name of data tab within data sheets? {bold(DATA_SHEET_NAME)}: "
-    )
-    if data_sheet_name:
-        DATA_SHEET_NAME = data_sheet_name
-
-
 if __name__ == "__main__":
+    print("\n{:-^30}".format("CONFIG"))
+    print("Press enter to use default values.\n")
     get_input()
+
+    print("\n{:-^30}".format("RUNNING"))
     main()
